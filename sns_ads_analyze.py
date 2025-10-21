a0de962356c3aba2f5ce06d4f9c9fad291285b6f
Kyotaro <k38life@gmail.com>
2025-10-22 01:27:46 +0900

    Ver1.6 : k=6でクラスタごとの特徴量と平均CTR・CVRを確認。簡単な考察を追加

--- sns_ads_analyze.py
+++ sns_ads_analyze.py
@@ -4,6 +4,7 @@
 
 # %% 必要なモジュールのインポート
 from tkinter.constants import X
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -11,8 +12,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
+from sklearn.manifold import TSNE
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+# %%  タイムスタンプを取得
+timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+print(timestamp)
 
 # %%  データセットの読み込み
 df_events = pd.read_csv('rawdata/ad_events.csv')
@@ -98,10 +104,14 @@ test_all = df_merged.loc[test_idx].reset_index(drop=True)
 
 # %%  クラスタリングに使用するカラムを選択
 use_cols = [
-"day_of_week","user_gender","user_age","hour_sin","hour_cos"
+"day_of_week","user_gender","user_age","hour_sin","hour_cos","Purchase","imp","click"
 ]
-X_train = train_all[use_cols]
-X_train = X_train.copy()
+X_train_s = train_all[use_cols]
+X_train = X_train_s.copy()
+
+#%%
+print(X_train.head(5))
+
 
 # %% 曜日を周期エンコーディング
 ## 曜日を数値化(マッピング)
@@ -122,10 +132,14 @@ X_train["weekday_sin"] = np.sin(2 * np.pi * X_train["weekday_num"] / 7)
 X_train["weekday_cos"] = np.cos(2 * np.pi * X_train["weekday_num"] / 7)
 X_train = X_train.drop(columns=["weekday_num","day_of_week"])
 
-# カテゴリカル変数（性別）をワンホットエンコーディング
+print(X_train.head(5))
+
+# %% カテゴリカル変数（性別）をワンホットエンコーディング
 cat_cols = ["user_gender"]
 X_train_encoded = pd.get_dummies(X_train, columns=cat_cols, drop_first=False, dtype=int)
 print(f'ワンホットエンコーディング結果：{X_train_encoded.head(10)}')
+
+print(X_train_encoded.head(5))
 
 # %% user_age を標準化
 scaler = StandardScaler()
@@ -138,62 +152,125 @@ std_age = X_train["user_age"].std()
 print(f'平均年齢：{mean_age}')
 print(f'年齢標準偏差：{std_age}')
 
+print(X_train_encoded.head(5))
+
 # %% データをCSVで確認
 # X_train_encoded.head(1000).to_csv("outputs/X_train_encoded_user_3_head1000.csv")
 
-# %% 訓練データをdfからarrayに変換
-X_train_arr = X_train_encoded.to_numpy()
+X_train_encoded_drop = X_train_encoded.copy()
+X_train_encoded_drop = X_train_encoded_drop.drop(["Purchase","imp","click"], axis=1)
+print(X_train_encoded.head(5))
+print(X_train_encoded_drop.head(5))
 
-# %% 比較のためにPCAを1回実行
+
+# %% 訓練データをdfからarrayに変換
+X_train_arr = X_train_encoded_drop.to_numpy()
+
+# kを6に設定
+k = 6
+
+# kmインスタンスを作成
+km = KMeans(n_clusters=k, init= "random", random_state=0, n_init='auto')
+# モデルの学習と予測を実行
+Y_km = km.fit_predict(X_train_arr)
+print(Y_km)
+
+# %%
+# PCAで特徴量を2次元に圧縮
 pca = PCA(n_components=2, random_state=0)
 X_pca = pca.fit_transform(X_train_arr)
 
-# 検証するk数を指定
-k_list = [4,6,8]
+# セントロイド（クラスタ中心）もPCA空間に変換
+centers_pca = pca.transform(km.cluster_centers_)
 
-# スコア格納リストを作成
-dbi_list, ch_list = [], []
-
-# グラフの描画領域を準備
-fig, axes = plt.subplots(1, 3, figsize=(15, 4), constrained_layout=True)
-
-for ax, k in zip(axes, k_list):
-    # kmインスタンスを作成
-    km = KMeans(n_clusters=k, init= "random", random_state=0, n_init="auto")
-    # モデルの学習と予測を実行
-    Y_km = km.fit_predict(X_train_arr)
-
-    # クラスタリングの評価
-    # silhouette = silhouette_score(X_train_arr, Y_km) #シルエットスコアは計算が重いので割愛
-    dbi = davies_bouldin_score(X_train_arr, Y_km)
-    ch = calinski_harabasz_score(X_train_arr, Y_km)
-
-    dbi_list.append(dbi)
-    ch_list.append(ch)
-
-    print(f"k={k} | DBI={dbi:.3f}, CH={ch:.1f}")
-
-    # 散布図を作成
-    sc = ax.scatter(X_pca[:, 0], X_pca[:, 1], c=Y_km, s=10, alpha=0.6)
-    ax.set_title(f"k={k} | DBI={dbi:.3f} ↓  CH={ch:.1f} ↑")
-    ax.set_xlabel("PC1")
-    ax.set_ylabel("PC2")    
-
-plt.suptitle("PCA Scatter by k (KMeans, common PCA space)", y=1.03, fontsize=12)
-plt.savefig("outputs/figures/lcustering_user_kmeans_pca_k4_6_8.png", dpi=300, bbox_inches='tight')
+# 散布図で可視化
+plt.figure(figsize=(6, 6))
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=Y_km, cmap='viridis', alpha=0.6)
+plt.scatter(centers_pca[:, 0], centers_pca[:, 1], 
+            c='red', s=200, marker='X', label='Centroids')
+plt.xlabel('PCA Component 1')
+plt.ylabel('PCA Component 2')
+plt.title('K-means Clusters_User_v1_ (PCA 2D Projection)')
+plt.legend()
+plt.savefig(f'outputs/figures/kmeans_clusters_user_k=6_{timestamp}.png', dpi=300, bbox_inches='tight')
 plt.show()
 
-# スコアをデータフレーム化
-score_df = pd.DataFrame({
-    "k": k_list,
-    "DBI": dbi_list,
-    "CH": ch_list
-})
+# %% クラスタリングの評価
+# silhouette = silhouette_score(X_train_arr, Y_km) #シルエットスコアは計算が重いので割愛
+dbi = davies_bouldin_score(X_train_arr, Y_km)
+ch = calinski_harabasz_score(X_train_arr, Y_km)
 
-score_df.to_csv("outputs/clustering__user_score_compare-k2.csv")
+# print(f"Silhouette Score: {silhouette:.3f}")
+print(f"Davies-Bouldin Index: {dbi:.3f}")
+print(f"Calinski-Harabasz Index: {ch:.3f}")
 
-# ベストスコアを確認
-best_k_dbi = score_df.loc[score_df["DBI"].idxmin(), "k"]
-best_k_ch = score_df.loc[score_df["CH"].idxmax(), "k"]
-print(f"・DBI最小 → DBI={score_df["DBI"].min()}, k={best_k_dbi}")
-print(f"・CH最大 → CH={score_df["CH"].max()}, k={best_k_ch}")
+# %% クラスタ番号をdf1に結合
+Y_km_s = pd.Series(Y_km, index=X_train_encoded.index, name="cluster")
+df = X_train_encoded.join(Y_km_s)
+df.head(10).to_csv(f"outputs/df_cluster_k=6_{timestamp}.csv")
+
+# %% 年齢を元に戻す（手計算）
+df["user_age"] = df["user_age"] * std_age + mean_age
+print(df["user_age"].head(30))
+
+# %% 時間を24時間表記に戻す
+df["hour"] = np.degrees(np.arctan2(df["hour_sin"], df["hour_cos"])) * (24 / 360)
+df["hour"] = (df["hour"] + 24) % 24
+print(df["hour"].head(30))
+
+# %% 曜日を元に戻す
+df["weekday_num"] = np.degrees(np.arctan2(df["weekday_sin"], df["weekday_cos"])) * (7 / 360)
+df["weekday_num"] = (df["weekday_num"] + 7) % 7  # 負の値補正
+df["weekday_num"] = df["weekday_num"].round().astype(int)
+
+# キーと値を入れ替えた辞書を作成（逆マップ）
+weekday_map_inv = {v: k for k, v in weekday_map.items()}
+
+df['weekday'] = df['weekday_num'].map(weekday_map_inv)
+print(df['weekday'].head(30))
+
+# %% 各クラスタの特徴量傾向を把握(曜日)
+# 件数集計
+ct = pd.crosstab(df["cluster"], df["weekday"])
+# 件数→割合（行方向に正規化）
+ct_pct = ct.div(ct.sum(axis=1), axis=0) 
+weekday_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+ct_pct = ct_pct[weekday_order]
+print(ct_pct)
+
+# %% クラスタ毎のCTR・CVR・CTVRを計算
+agg = (
+    df
+    .groupby("cluster", dropna=False)
+    .agg(
+        impressions=("imp", "sum"),
+        clicks=("click", "sum"),
+        conversions=("Purchase", "sum")
+    )
+)
+
+agg["CTR"] = np.where(agg["impressions"] > 0,
+                      agg["clicks"] / agg["impressions"], np.nan)
+agg["CVR"] = np.where(agg["clicks"] > 0,
+                      agg["conversions"] / agg["clicks"], np.nan)
+agg["CTVR"] = np.where(agg["impressions"] > 0,
+                      agg["CTR"] * agg["CVR"]*100, np.nan)
+print(agg.reset_index())
+print(agg)
+#%%
+
+# %% 各クラスタの特徴量傾向を把握(数値の平均値)
+cluster_summary = df.assign(cluster=Y_km).groupby("cluster").mean(numeric_only=True)
+print(cluster_summary)
+
+# %% 全出力をマージ
+df_cluster_feat = pd.concat([cluster_summary,ct_pct,agg],axis=1)
+print(df_cluster_feat)
+
+# %%  不要な列を削除
+drop_list = ["hour_sin","hour_cos","Purchase","imp","click","weekday_sin","weekday_cos"]
+df_cluster_feat_drop = df_cluster_feat.copy()
+df_cluster_feat_drop = df_cluster_feat_drop.drop(columns=drop_list,axis=1)
+print(df_cluster_feat_drop)
+df_cluster_feat_drop.to_csv(f"outputs/df_cluster_feat_k=6_{timestamp}.csv")
+# %%
